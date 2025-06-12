cbd2cfeaa8b9ec6b6adc62f07b4d8adb102cfbb7
Ondrej Vit <123187002+hwllffrdd@users.noreply.github.com>
2025-06-12 09:42:20 +0200

    Update run_combined_depth_bash.sh

--- scripts/run_combined_depth_bash.sh
+++ scripts/run_combined_depth_bash.sh
@@ -1,14 +1,10 @@
-# Create a directory for results
-mkdir -p ~/data/analysis
-
-# Create a list of all BAM files
-find ~/data/fastq/aligned_last -name "*.sorted.bam" > ~/data/analysis/all_bams.list
-
-# Run GATK DepthOfCoverage on all samples
+WORKDIR="/home/rstudio/data"
+mkdir -p $WORKDIR/analysis
+find $WORKDIR/fastq/aligned_last -name "*.sorted.bam" > $WORKDIR/analysis/all_bams.list
 gatk --java-options "-Xmx8g" DepthOfCoverage \
     -R $REF_GENOME \
-    -I ~/data/analysis/all_bams.list \
-    -L ~/data/pms2_targets.bed \
-    -O ~/data/analysis/combined_coverage \
+    -I $WORKDIR/analysis/all_bams.list \
+    -L $WORKDIR/pms2_targets.bed \
+    -O $WORKDIR/analysis/combined_coverage \
     --omit-depth-output-at-each-base \
     --include-ref-n-sites
